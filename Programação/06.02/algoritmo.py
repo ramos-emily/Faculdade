6208b7bd078804682c2b7b324acf6d2f11b4596f
ramos-emily <ramos.emilyvi@gmail.com>
2025-02-18 21:24:39 -0300

    livroResumo

--- Programação/06.02/algoritmo.py
+++ Programação/06.02/algoritmo.py
@@ -3,3 +3,13 @@
 # 1. definição do problema
 # 2. Projetar a solução(algoritmo)
 # 3. Codificar a solução(programar em uma linguagem)
+
+
+
+# def fibonacci(n):
+#     if n <= 1:
+#         return n
+#     return fibonacci(n - 1) + fibonacci(n - 2)
+
+# for i in range(10):
+#     print(fibonacci(i))
