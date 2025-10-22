93bd84173bb008b54acf27d8311fee14d3f92e4b
ramos-emily <ramos.emilyvi@gmail.com>
2025-10-21 22:32:59 -0300

    feat(git): url and create arquivos of the github ativ

---
+++ Programação/git/1.py
@@ -0,0 +1,16 @@
+# Questão 1
+# Dado um conjunto de inteiros nums e um inteiro alvo, retorne os índices dos dois números de forma que a soma deles seja o alvo. Você pode assumir que cada entrada terá exatamente uma solução e não pode usar o mesmo elemento duas vezes. Deverá ser construída uma função (chamada f) que receberá os dois argumentos nums e alvo.
+
+# Exemplo 1
+# Entrada: nums = [2, 7, 11, 15], alvo = 9
+# Saída: (0, 1)
+# Exemplo 2
+# Entrada: nums = [3, 2, 4], alvo = 6
+# Saída: (1, 2)
+# Exemplo 3
+# Entrada: nums = [3, 3], alvo = 6
+# Saída: (0, 1)
+
+nums = [2, 7, 11, 15]
+alvo = 9
+print(0, 1)
---
+++ Programação/git/3.py
@@ -0,0 +1,12 @@
+# Questão 3
+# Construa uma função que faça uma busca em uma lista de tuplas em que tem na primeira posição um número inteiro e na segunda um nome. A função (chamada f) deverá retornar o nome após receber como argumentos uma lista e um número inteiro que deverá ser usado na busca pelo nome.
+
+# Exemplo 1
+# Entrada: lista = [(18, 'Ana'), (10, 'Bruno'), (15, 'Carlos'), (3, 'Daniela')] e busca = 15
+# Saída: Carlos
+# Exemplo 2
+# Entrada: lista = [(187, 'Isabela'), (13, 'João'), (199, 'Karen'), (201, 'Leonardo')] e busca = 199
+# Saída: Karen
+
+list = [(18, 'Ana'), (10, 'Bruno'), (15, 'Carlos'), (3, 'Daniela')]
+busca = 15
